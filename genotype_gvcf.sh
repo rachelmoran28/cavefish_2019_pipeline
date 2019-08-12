1311eddb59a42dba6ff970bd16578b746dc4a0e7
rmoran <rmoran@umn.edu>
2019-08-12 17:04:26 -0500

    Update genotype_gvcf.sh

--- genotype_gvcf.sh
+++ genotype_gvcf.sh
@@ -12,13 +12,16 @@ date
 
 # Load modules
 module load java
-#module load gatk/3.7.0 #used for V1 genome for compatibility with Eli's stuff.
+module load gatk/3.7.0 #used for V1 genome for compatibility with Eli's stuff.
 
 # Set paths
 #   V1
 REF="/home/mcgaughs/shared/References/Astyanax_mexicanus_102/Astyanax_mexicanus.AstMex102.dna.toplevel.fa"
 IODIR="/panfs/roc/scratch/aherman/RIS_cavefish/old_genome_mapping"
+
+#fix path to GATK
 GATK=/panfs/roc/groups/14/mcgaughs/shared/Software/GenomeAnalysisTK-3.8-0-ge9d806836/GenomeAnalysisTK.jar
+
 VCFS=($(find ${IODIR}/GVCFs -type f -name *.g.vcf))
 
 GATK_IN=()
@@ -33,7 +36,7 @@ java -Djava.io.tmpdir=/scratch.local \
     -jar ${GATK} \
     -T GenotypeGVCFs \
     -R ${REF} \
-    -L ${IODIR}/vcf_restart3_intervals.list \ . ### What is "vcf_restart3_intervals.list"?
+    -L ${IODIR}/vcf_restart3_intervals.list \ . ### What is "vcf_restart3_intervals.list"? Scaffold list?
     ${GATK_IN[@]} \
     -o ${IODIR}/Astyanax_v1genome_ahsamples_restart3.full.vcf \
     -nt 32 \
