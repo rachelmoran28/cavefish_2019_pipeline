c29903b5c7fb775eefc03e5c57417519f29b3901
rmoran <rmoran@umn.edu>
2019-08-21 14:32:15 -0500

    Update genotype_gvcf_v1.sh

--- genotype_gvcf_v1.sh
+++ genotype_gvcf_v1.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-#PBS -l mem=998gb,nodes=1:ppn=32,walltime=96:00:00
+#PBS -l mem=126gb,nodes=1:ppn=16,walltime=96:00:00
 #PBS -m abe
 #PBS -M rmoran@umn.edu
-#PBS -q ram1t
-#PBS -A mcgaughs
+#PBS -q sb128
 #PBS -N genomeV1_genotyping
 #PBS -j oe
 
@@ -17,12 +16,12 @@ module load gatk/3.7.0 #used for V1 genome for compatibility with Eli's stuff.
 # Set paths
 #   V1
 REF="/home/mcgaughs/shared/References/Astyanax_mexicanus_102/Astyanax_mexicanus.AstMex102.dna.toplevel.fa"
-IODIR="/panfs/roc/scratch/aherman/RIS_cavefish/old_genome_mapping"
+IODIR="/home/mcgaughs/shared/Datasets/per_individual.g.vcfs/v1_cavefish"
 
 #fix path to GATK
 GATK=/panfs/roc/msisoft/gatk/3.7.0/GenomeAnalysisTK.jar
 
-VCFS=($(find ${IODIR}/GVCFs -type f -name *.g.vcf))
+VCFS=($(find ${IODIR}/GVCFs -type f -name *.g.vcf.gz))
 
 GATK_IN=()
 
@@ -31,6 +30,7 @@ do
 	GATK_IN+=("-V $f")
 done
 
+
 export _JAVA_OPTIONS="-Xmx990g"
 java -Djava.io.tmpdir=/scratch.local \
     -jar ${GATK} \
@@ -38,8 +38,8 @@ java -Djava.io.tmpdir=/scratch.local \
     -R ${REF} \
     -L ${IODIR}/vcf_restart3_intervals.list \ . ### Scaffold list? In the v2 script this list includes Chr 1-25 and then Chr Un (all unplaced scaffolds get grouped together?)
     ${GATK_IN[@]} \
-    -o ${IODIR}/Astyanax_v1genome_ahsamples_restart3.full.vcf \ . ### Is this line going to name each sample's vcf the same thing?? In the v2 script it looks like each vcf gets a unique ID
-    -nt 32 \
+    -o ${IODIR}/Astyanax_v1genome_183_samples.full.vcf \ . ### Is this line going to name each sample's vcf the same thing?? In the v2 script it looks like each vcf gets a unique ID
+    -nt 24 \
     --includeNonVariantSites
 
 echo -n "Done: "
