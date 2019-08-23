75fd35bad3507376222689127ad3cda9d97e50d6
rmoran <rmoran@umn.edu>
2019-08-23 16:30:32 -0500

    Create combine_gvcfs.sh

---
+++ combine_gvcfs.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+#PBS -l mem=16gb,nodes=1:ppn=1,walltime=48:00:00
+#PBS -m abe
+#PBS -M rmoran@umn.edu
+#PBS -q mesabi
+
+# Load modules
+module load java
+
+# Set paths
+GATK="/panfs/roc/groups/14/mcgaughs/shared/Software/GenomeAnalysisTK-3.8-0-ge9d806836/GenomeAnalysisTK.jar"
+#   V2
+REF="/home/mcgaughs/shared/References/2017-09_Astyanax_mexicanus/7994_ref_Astyanax_mexicanus-2.0_Complete_gbID.fa"
+GVCF_DIR="/home/mcgaughs/shared/Datasets/per_individual.g.vcfs/v2_surfacefish/gvcfs"
+OUTPUT_DIR="/home/mcgaughs/shared/Datasets/all_sites_LARGE_gvcf/raw_surfacefish/combined_gvcf"
+
+# We will combine GVCFs based on population
+#POPS=("Chica" "Choy" "Molino" "Pach" "Rascon" "Tinaja")
+#CURR_POP=${POPS[${PBS_ARRAYID}]}
+
+# Build the sample list
+SAMPLE_LIST=($(find ${GVCF_DIR} -name '*.g.vcf.gz' | sort -V))
+# Put them into a format that will be accepted by the GATK command line
+GATK_IN=()
+for s in "${SAMPLE_LIST[@]}"
+do
+	GATK_IN+=("-V $s")
+done
+
+mkdir -p ${OUTPUT_DIR}
+
+export _JAVA_OPTIONS="-Xmx15000m -Djava.io.tmpdir=/panfs/roc/scratch/rmoran/java_tmp"
+java -jar ${GATK} \
+    -T CombineGVCFs \
+    -R ${REF} \
+    ${GATK_IN[@]} \
+    -o ${OUTPUT_DIR}/Surface_Combined.g.vcf.gz
