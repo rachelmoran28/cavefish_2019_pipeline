99b1d1be8ab0b09bcb7615cda9583cda10f762f5
rmoran <rmoran@umn.edu>
2019-08-23 17:40:55 -0500

    Update combine_gvcfs.sh

--- combine_gvcfs.sh
+++ combine_gvcfs.sh
@@ -3,6 +3,7 @@
 #PBS -m abe
 #PBS -M rmoran@umn.edu
 #PBS -q mesabi
+#PBS t 1-21
 
 # Load modules
 module load java
@@ -14,17 +15,18 @@ REF="/home/mcgaughs/shared/References/2017-09_Astyanax_mexicanus/7994_ref_Astyan
 GVCF_DIR="/home/mcgaughs/shared/Datasets/per_individual.g.vcfs/v2_surfacefish/gvcfs"
 OUTPUT_DIR="/home/mcgaughs/shared/Datasets/all_sites_LARGE_gvcf/raw_surfacefish/combined_gvcf"
 
-# We will combine GVCFs based on population
-#POPS=("Chica" "Choy" "Molino" "Pach" "Rascon" "Tinaja")
-#CURR_POP=${POPS[${PBS_ARRAYID}]}
+#Combine GVCFs based on population
+POPS=("Aeneus" "Arroyo" "Asty" "CabMoro" "Chica1" "Chica5" "Choy" "Escon" "Gallinas" "Jineo" "Mante" "Molino" "Nicara" "Pachon" "Peroles" "Rascon" "Subter" "Surface" "Tigre" "Tinaja" "Vasquez")
+CURR_POP=${POPS[${PBS_ARRAYID}]}
 
 # Build the sample list
-SAMPLE_LIST=($(find ${GVCF_DIR} -name '*.g.vcf.gz' | sort -V))
+SAMPLE_LIST=($(find ${GVCF_DIR} -name '*.g.vcf.gz' | grep ${CURR_POP} |  sort -V))
+
 # Put them into a format that will be accepted by the GATK command line
 GATK_IN=()
 for s in "${SAMPLE_LIST[@]}"
 do
-	GATK_IN+=("-V $s")
+    GATK_IN+=("-V $s")
 done
 
 mkdir -p ${OUTPUT_DIR}
@@ -34,4 +36,4 @@ java -jar ${GATK} \
     -T CombineGVCFs \
     -R ${REF} \
     ${GATK_IN[@]} \
-    -o ${OUTPUT_DIR}/Surface_Combined.g.vcf.gz
+    -o ${OUTPUT_DIR}/${CURR_POP}_Comb.g.vcf.gz
